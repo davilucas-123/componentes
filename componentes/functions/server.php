51175f8f01ec166a2a19ab66d8f001765599986e
davilucas-123 <davilucasdaniel2009@gmail.com>
2026-09-16 15:34:02 -0300

    Delete componentes directory

--- componentes/functions/server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function servidorHttp(){
-        echo "1. Servidor HTTP recebeu a requisição.<br>";
-    router();
-}
--- componentes/services/usuario_service.php
+++
@@ -1,9 +0,0 @@
-<?php
-function usuarioService(){
-    echo "7. Service está executando a regra de negócio.<br>"
-    return [
-        "João"
-        "Maria"
-        "Carlos"
-    ]
-}
