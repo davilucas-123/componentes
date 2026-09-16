4db19aa72350c006ad460efca1fcaf2e240891e3
Miguel Felipe Gomes Silva <miguelfelipegs>
2026-09-16 15:30:27 -0300

    atualizado1

---
+++ middleware/middleware.php
@@ -0,0 +1,18 @@
+<?php
+
+function middleware($rota){
+    echo "3. Middleware está verificando a requisição.<br>";
+    $permitido = true;
+
+    if ($permitido){
+        echo "4. Middleware permitiu continuar.<br>";
+        dispacter($rota);
+
+
+    } else {
+        echo "4. Middleware bloqueou a requisição.<br>";
+    }
+
+
+    }
+}
