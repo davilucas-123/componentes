620db6b785b0d3b606c2cccb53dc1869a967a4ed
davilucas-123 <davilucasdaniel2009@gmail.com>
2026-09-16 15:33:38 -0300

    Add files via upload

---
+++ componentes/dispatcher/dispatcher.php
@@ -0,0 +1,10 @@
+<?php
+
+function dispatcher($rota){
+    echo "5.Dispatcher decidiu qual controller deve executar.<br>";
+    if ($rota === "/usuarios"){
+        usuarioController();
+    }
+
+
+}
---
+++ componentes/services/usuario_service.php
@@ -0,0 +1,9 @@
+<?php
+function usuarioService(){
+    echo "7. Service está executando a regra de negócio.<br>"
+    return [
+        "João"
+        "Maria"
+        "Carlos"
+    ]
+}
