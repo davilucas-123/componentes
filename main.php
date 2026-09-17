9d55c39bd38dbe9f8721c644cba3e3f11f63fc58
davilucas-123 <davilucasdaniel2009@gmail.com>
2026-09-17 13:48:55 -0300

    Initialize main.php with required files

---
+++ main.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/functions/server.php';
+require_once __DIR__ . '/router/routerr.php';
+require_once __DIR__ . '/middleware/middleware.php';
+require_once __DIR__ . '/dispatcher/dispatcher.php';
+require_once __DIR__ . '/controllers/controllers.php';
+require_once __DIR__ . '/services/usuario_services.php';
